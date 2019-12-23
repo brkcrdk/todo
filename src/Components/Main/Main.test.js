3edd31ff7262f4fc67249fd03081ac93bd6c2bab
Burak <brkcrdk@gmail.com>
2019-12-23 19:42:24 +0200

    Cleaning

--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Main from "./Main";
 
-test("render Main component", () => {
+test("render Main components header", () => {
   const { getByText } = render(<Main />);
   const headerElement = getByText(/Main page goes here/i);
   expect(headerElement).toBeInTheDocument();

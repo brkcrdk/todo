bf44b2ccd4e2446447e7e2db2c1075e87e721649
Burak <brkcrdk@gmail.com>
2019-12-23 16:33:03 +0200

    Cleaning

--- src/App.test.js
+++ src/App.test.js
@@ -7,12 +7,3 @@ test("renders learn react link", () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test("renders button", () => {
-  const { getByText } = render(<App />);
-  const buttonLabel = getByText(/Your first/i);
-  const buttonElement = getByText(/My first test/i);
-
-  expect(buttonLabel).toBeInTheDocument();
-  expect(buttonElement).toBeInTheDocument();
-});

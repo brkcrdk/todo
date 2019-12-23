0614c4b63d58504f647cd605b9d6abe9d494f881
Burak <brkcrdk@gmail.com>
2019-12-23 18:26:17 +0200

    Testing

--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -4,4 +4,6 @@ import Main from "./Main";
 
 test("render Main component", () => {
   const { getByText } = render(<Main />);
+  const headerElement = getByText(/Main page goes here/i);
+  expect(headerElement).toBeInTheDocument();
 });

d1dfec4bd4eaa38a44e86006aec53b979a8b22d8
Burak <brkcrdk@gmail.com>
2019-12-23 18:24:25 +0200

    Testing

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h3>Main page goes here</h3>
+    </div>
+  );
 };
 
 export default Main;
--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -3,5 +3,5 @@ import { render } from "@testing-library/react";
 import Main from "./Main";
 
 test("render Main component", () => {
-  expect(true).toBeTruthy();
+  const { getByText } = render(<Main />);
 });

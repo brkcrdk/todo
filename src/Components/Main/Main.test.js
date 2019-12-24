2d01a3515bd163c6d08f90eba30fdb1876ce4ecc
Burak <brkcrdk@gmail.com>
2019-12-24 14:38:33 +0200

    Bug fix

--- src/Components/Main/Main.test.js
+++ src/Components/Main/Main.test.js
@@ -3,8 +3,8 @@ import { render } from "@testing-library/react";
 import Main from "./Main";
 
 const setup = () => {
-  const { utils, getByTestId } = render(<Main />);
-  const main = getByTestId("main-container");
+  const utils = render(<Main />);
+  const main = utils.getByTestId(/main-container/gi);
   return {
     main,
     ...utils
@@ -12,14 +12,13 @@ const setup = () => {
 };
 
 test("render Main components header", () => {
-  expect(true).toBe(true);
-  // const { getByText } = setup();
-  // const headerElement = getByText(/Main page goes here/i);
-  // expect(headerElement).toBeInTheDocument();
+  const { getByText } = setup();
+  const headerElement = getByText(/Main page goes here/i);
+  expect(headerElement).toBeInTheDocument();
 });
 
 test("render item component", () => {
-  // const { getByTestId } = setup();
-  // const itemElement = getByTestId(/item-container/i);
-  // expect(itemElement).toBeInTheDocument();
+  const { getByTestId } = setup();
+  const itemElement = getByTestId(/item-container/i);
+  expect(itemElement).toBeInTheDocument();
 });

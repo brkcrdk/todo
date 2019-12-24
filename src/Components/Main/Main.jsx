70bea3407138a816925571fd512e10a363702876
Burak <brkcrdk@gmail.com>
2019-12-24 15:12:39 +0200

    Cleaning

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,10 +3,15 @@ import Item from "../TodoItem/Item";
 const Main = () => {
   const [todo, setTodo] = useState([]);
   const [value, setValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div data-testid="main-container">
-      <h3>Main page goes here</h3>
-      <Item />
+      <h3>Main page goes here {value}</h3>
+      <Item value={value} change={handleChange} />
     </div>
   );
 };
--- src/Components/TodoItem/Item.jsx
+++ src/Components/TodoItem/Item.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Item = ({ value }) => {
+const Item = ({ value, change }) => {
   return (
     <form data-testid="item-container">
       <label>Add Todo</label>
-      <input value={value} aria-label="todo-input" />
+      <input value={value} aria-label="todo-input" onChange={change} />
     </form>
   );
 };

8bce8dacc1fc08d6f2c52255395688f3376c6226
Burak <brkcrdk@gmail.com>
2019-12-27 14:29:21 +0200

    Cleaning

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders Main page", () => {
-  const { getByTestId } = render(<App />);
-  const mainComponent = getByTestId(/main-container/gi);
-  expect(mainComponent).toBeInTheDocument();
-});

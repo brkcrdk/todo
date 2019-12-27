a2dd50303e80a16ef8a36c79ffcf24f0d1cd9a2a
Burak <brkcrdk@gmail.com>
2019-12-27 15:38:05 +0200

    Swap function added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,19 +1,29 @@
-import React, { useState, useCallback } from "react";
+import React, { useState } from "react";
 
 const Main = () => {
   const [arr, setArr] = useState([1, 2, 3, 4, 5, 6]);
   const swap = (array, idx1) => {
-    let temp = array[idx1];
-    array[idx1] = array[idx1 + 1];
-    array[idx1 + 1] = temp;
-    return array;
+    const newArray = array.slice();
+    newArray[idx1] = array[idx1 + 1];
+    newArray[idx1 + 1] = array[idx1];
+    return newArray;
   };
-
+  const handleClick = (index) => {
+    console.log(swap(arr, index));
+    const newArry = swap(arr, index);
+    setArr(newArry);
+  };
   return (
     <div data-testid="main-container">
       <h3>Main page goes here</h3>
       {arr.map((item, i) => (
-        <button key={i}>{item}</button>
+        <button
+          key={i}
+          onClick={() => {
+            handleClick(i);
+          }}>
+          {item}
+        </button>
       ))}
     </div>
   );

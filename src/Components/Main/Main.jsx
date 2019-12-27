ac55b0e57742ec08bc12344c36314c1af557945e
Burak <brkcrdk@gmail.com>
2019-12-27 14:31:15 +0200

    Cleaning

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,24 +1,9 @@
-import React, { useState } from "react";
-import List from "../List/List";
-import Item from "../Item/Item";
-const Main = () => {
-  const [todo, setTodo] = useState([{ job: "Gym" }, { job: "Run" }]);
-  const [value, setValue] = useState("");
+import React from "react";
 
-  const handleClick = (e) => {
-    if (value !== "") {
-      setTodo([...todo, { job: value }]);
-      setValue("");
-    }
-  };
-  const handleChange = (e) => {
-    setValue(e.target.value.trim());
-  };
+const Main = () => {
   return (
     <div data-testid="main-container">
       <h3>Main page goes here</h3>
-      <Item value={value} onChange={handleChange} onClick={handleClick} />
-      <List todos={todo} />
     </div>
   );
 };

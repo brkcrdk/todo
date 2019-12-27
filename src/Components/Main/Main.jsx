d74246a4cb058cdd195b0aa14ffd2efc3c784dcd
Burak <brkcrdk@gmail.com>
2019-12-27 15:15:26 +0200

    swap func added

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Main = () => {
+  const arr1 = [1, 2, 3, 4, 5, 6];
+  const swap = (array, idx1) => {
+    let temp = array[idx1];
+    array[idx1] = array[idx1 + 1];
+    array[idx1 + 1] = temp;
+    return array;
+  };
+  const handleSwap = () => {};
   return (
     <div data-testid="main-container">
       <h3>Main page goes here</h3>
